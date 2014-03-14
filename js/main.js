10596fec864ace171bf94a9a90713d7a313875cb
Ellison Leão <ellisonleao@gmail.com>
2014-03-14 02:49:06 -0300

    add more stuff :smile:

--- js/main.js
+++ js/main.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
     invertY: true,
     limitX: false,
   });
+
+  // smooth scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
